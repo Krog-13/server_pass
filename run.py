238ecea0bab8e85b4a2371670eb33cc073642c9f
Krog-13 <virtualspook@gmail.com>
2023-03-31 11:12:32 +0600

    three

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from wsgiref.simple_server import make_server
 import datetime
 from src.app import application
 
+def func():
+    pass
+
 if __name__ == '__main__':
     with make_server('', 5001, application) as httpd:
         print(" * Serving on http://{0}:{1} (Press CTRL+C to quit)"
