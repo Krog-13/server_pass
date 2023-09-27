ab1dc154acfa4015c31939078e62422ad01a7d76
Krog-13 <52882802+Krog-13@users.noreply.github.com>
2023-09-27 09:55:30 +0600

    Update run.py port 5003

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def func():
     date = datetime.date(2014,12,12)
 
 if __name__ == '__main__':
-    with make_server('', 5005, application) as httpd:
+    with make_server('', 5003, application) as httpd:
         print(" * Serving on http://{0}:{1} (Press CTRL+C to quit)"
               .format(*httpd.server_address))
         try:
